2920bd233b9c35fcb679d3f0f52b01d52430a2e7
jyotimahan22 <jyotimahan22@gmail.com>
2022-03-30 22:32:31 +0530

    First test by X

---
+++ DemoGitHub/src/Test/Test2.java
@@ -0,0 +1,13 @@
+package Test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int r=a+b;
+		System.out.println("Result:"+r);
+	}
+
+}
